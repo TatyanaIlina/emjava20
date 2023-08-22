3327da239e918c88ae9fcf042643adb53338f5f0
T_T <ice257@yandex.ru>
2023-08-22 16:39:24 +0300

    f

---
+++ src/main/java/com/example/emjava20/Emjava20Application.java
@@ -0,0 +1,13 @@
+package com.example.emjava20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Emjava20Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Emjava20Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/emjava20/Application.java
@@ -0,0 +1,13 @@
+package com.example.emjava20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+    }
+
+}
